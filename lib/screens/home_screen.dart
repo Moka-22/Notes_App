77cc9e1e7879c71551d19148218fe895492b80de
Eslam Amr <islamamr622@gmail.com>
2024-04-29 00:18:44 +0300

    change theme and create home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notesappp/widgets/add_note_widget.dart';
+import 'package:notesappp/widgets/notes_item_widget.dart';
+import 'package:notesappp/widgets/notes_list.dart';
+import 'package:notesappp/widgets/notes_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +15,22 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.grey,
+        onPressed: (){
+          showModalBottomSheet(context: context, builder: (context){
+            return AddNoteButton();
+          });
+        },
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+      body: NotesWidgets(),
+    );
   }
 }
+
+
+
